0874ede893d5656442b16bb2a690598655e3da2f
Rye <ryenguyen7411@gmail.com>
2019-02-16 13:00:46 +0700

    UPDATE: implementing checkout page.

--- wp-content/themes/twentynineteen/template-parts/assets/zoe/js/main.js
+++ wp-content/themes/twentynineteen/template-parts/assets/zoe/js/main.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
-  $('select#color').after('<i class="fas fa-chevron-down"></i>')
+  function rearrange() {
+    console.log('REARRANGE')
+    $('select#color').after('<i class="fas fa-chevron-down"></i>')
 
-  $('.quantity > input[type=number]').before('<i class="fas fa-minus"></i>')
-  $('.quantity > input[type=number]').after('<i class="fas fa-plus"></i>')
+    $('.quantity > input[type=number]').before('<i class="fas fa-minus"></i>')
+    $('.quantity > input[type=number]').after('<i class="fas fa-plus"></i>')
 
-  $('.quantity >.fa-minus').click(function () { this.nextElementSibling.stepDown() })
-  $('.quantity >.fa-plus').click(function () { this.previousElementSibling.stepUp() })
+    $('.quantity >.fa-minus').click(function () { this.nextElementSibling.stepDown() })
+    $('.quantity >.fa-plus').click(function () { this.previousElementSibling.stepUp() })
 
-  $('.woocommerce-cart-form > table').before('<h4 class="cart-title">Cart</h4>')
+    $('.woocommerce-cart-form > table').before('<h4 class="cart-title">Cart</h4>')
+
+    $('.return-to-shop').after(`<p class="return-to-homepage">
+      <a class="button btn-primary" href="/">
+        Return to homepage</a>
+    </p>`)
+
+    const couponElem = $('.woocommerce-cart-form__contents .actions > .coupon')
+    couponElem.children('button[type=submit]').text('Apply')
+    $('.wc-proceed-to-checkout').before(couponElem)
+  }
+
+  rearrange()
+  $('input[type=number]').change(() => {
+    console.log('TRIGGER')
+    rearrange()
+  })
 });
