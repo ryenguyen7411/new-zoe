3909825e1f26486a4086b70738cf6d51201d95ae
Rye <ryenguyen7411@gmail.com>
2019-04-09 01:48:09 +0700

    UPDATE: add contact page.

---
+++ wp-content/themes/twentynineteen/template-zoe-form.php
@@ -0,0 +1,24 @@
+<?php /* Template Name: ZoeContactPage */
+
+$template_slug = 'zoe';
+get_header($template_slug);
+?>
+
+<section class="page-content mw-720">
+  <div class="container">
+    <h3><?php the_title(); ?></h3>
+    <?php
+  // TO SHOW THE PAGE CONTENTS
+    while ( have_posts() ) : the_post(); ?>
+    <?php the_content(); ?>
+    <?php
+  endwhile; //resetting the page loop
+  wp_reset_query(); //resetting the page query
+  ?>
+
+    <?php echo do_shortcode('[wpforms id="106" title="false" description="false"]') ?>
+  </div>
+</section>
+
+<?php
+get_footer($template_slug);
